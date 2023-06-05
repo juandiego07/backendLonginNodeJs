3ae75deabb7aab8eb960bc5a12e59b128a4c6683
juandiego07 <juan_diego07@hotmail.com>
2023-06-04 21:04:48 -0500

    Create login controllers

---
+++ controllers/login.js
@@ -0,0 +1,50 @@
+const bcrypt = require("bcrypt");
+const modelUser = require("../model/users");
+
+const login = async (req, res) =>  {
+
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.json({
+      status: false,
+      message: "The filds email or password are required",
+    });
+  } else {
+    modelUser
+      .findOne({ where: { email: email } })
+      .then((user) => {
+        if (!user) {
+          res.json({ status: false, message: "User no found" });
+        } else {
+          bcrypt
+            .compare(password, user.getDataValue("password"))
+            .then((isCorrect) => {
+              if (isCorrect) {
+                res.json({
+                  status: true,
+                  message: "User logged",
+                  data: {
+                    id: user.getDataValue("id"),
+                    name: user.getDataValue("name"),
+                    lastName: user.getDataValue("lastName"),
+                    role: user.getDataValue("role"),
+                  },
+                });
+              } else {
+                res.json({ status: false, message: "Password invalid" });
+              }
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+};
+
+module.exports = login;
