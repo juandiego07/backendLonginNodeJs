92fd20f47c8453a8b4bb1a3ad35d0000560485e9
juandiego07 <juan_diego07@hotmail.com>
2023-06-05 20:45:42 -0500

    Add getAllUsers

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const login = require ("./login");
 const register = require("./register");
+const getAllUsers = require("./getAllUsers");
 
 module.exports = {
   login,
   register,
+  getAllUsers,
 };
