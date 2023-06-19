22759d88ab4cb89296c94ee970eb4192bc649b17
juandiego07 <juan_diego07@hotmail.com>
2023-06-19 18:43:59 -0500

    Include new sdk

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+var cookieParser = require("cookie-parser");
 const controllers = require("./controllers/");
 const dataBase = require("./database/dataBase");
 
+
 const app = express();
-app.use(express.json(), cors());
+var cookieParser = require("cookie-parser");
+app.use(express.json(), cors(), cookieParser());
 
 // Registro de usaurio
 app.post("/register", controllers.register);
@@ -15,10 +18,11 @@ app.post("/login", controllers.login);
 // Se obtienen todos los usuarios
 app.post("/getallusers", controllers.getAllUsers);
 
+
 app.listen(5000, async () => {
   console.log("Server running http://localhost:5000");
   try {
-   await dataBase.authenticate();
+    await dataBase.authenticate();
   } catch (error) {
     console.error(error);
   }
